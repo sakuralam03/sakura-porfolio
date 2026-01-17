3ee228da3f809f90fb93aa2a42b953aa4071f2e1
Sakura Lam <1007871@mymail.sutd.edu.sg>
2026-01-17 22:42:15 +0800

    update project page

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,33 +1,38 @@
 // ProjectsSection.jsx
 import React from 'react';
 import "../styles/ProjectsSection.css";
-import WebDevelopment from "../pages/webdevelopment";
 
+// ✅ Import each image properly
+import webDevImg from "../assets/dcdbd311b1424b0d2e063f1dd82a65715cf6de04.png";
+import cyberSecImg from "../assets/d738bb246dfa6444676ea523629ed3ca0c4e1b0a.png";
+import gameDevImg from "../assets/101ff07299229563730e0bb60c6f5610e77851d8.png";
+import appDevImg from "../assets/6a5139c32c47b37421efef6da39c0d9104e89a18.png";
+import otherProjImg from "../assets/161479c00fcb23413951c68e0ab638903df5f9f5.png";
 
 const projects = [
   {
     title: 'Web Development Projects',
-    image: 'src/assets/dcdbd311b1424b0d2e063f1dd82a65715cf6de04.png',
+    image: webDevImg,
     link: '/webdevelopment',
   },
   {
     title: 'CyberSecurity Projects',
-    image: 'src/assets/d738bb246dfa6444676ea523629ed3ca0c4e1b0a.png',
+    image: cyberSecImg,
     link: '/cybersecurity',
   },
   {
     title: 'Game Development Projects',
-    image: 'src/assets/101ff07299229563730e0bb60c6f5610e77851d8.png',
+    image: gameDevImg,
     link: '/gamedevelopment',
   },
   {
     title: 'App Development Projects',
-    image: 'src/assets/6a5139c32c47b37421efef6da39c0d9104e89a18.png',
+    image: appDevImg,
     link: '/appdevelopment',
   },
   {
     title: 'Other Projects',
-    image: 'src/assets/161479c00fcb23413951c68e0ab638903df5f9f5.png',
+    image: otherProjImg,
     link: '/otherprojects',
   },
 ];
